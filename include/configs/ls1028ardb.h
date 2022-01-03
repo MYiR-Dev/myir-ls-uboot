afd0919bffa8e560d75759e9353d4f82597baef5
wujl <1053924360@qq.com>
2022-01-03 23:08:24 +0800

    FIX:remove fix boot scripts

--- include/configs/ls1028ardb.h
+++ include/configs/ls1028ardb.h
@@ -112,11 +112,9 @@
 	"kernelhdr_addr_sd=0x3000\0"		\
 	"kernelhdr_size_sd=0x20\0"		\
 	"console=ttyS0,115200\0"                \
-	"devnum=1\0"                              \
 	"mtdparts=" CONFIG_MTDPARTS_DEFAULT "\0"	\
 	BOOTENV					\
 	JAILHOUSE_ENV \
-	"boot_scripts=mydjls1028_boot.scr\0"    \
 	"boot_script_hdr=hdr_ls1028ardb_bs.out\0"	\
 	"scan_dev_for_boot_part="               \
 		"part list ${devtype} ${devnum} devplist; "   \
